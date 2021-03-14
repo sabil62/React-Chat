8a028f81aeb594d581cfc5ef9c1f0c2378e38742
sabil62 <shresthasabil62@gmail.com>
2021-03-14 12:50:53 +0545

    logout and app successfully deployed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { ChatEngine } from "react-chat-engine";
 import ChatFeed from "./components/chatFeed";
 import LoginForm from "./components/loginForm";
+import React from "react";
 
 const App = () => {
   if (!localStorage.getItem("userName")) {
@@ -12,16 +13,44 @@ const App = () => {
     var localUser = localStorage.getItem("userName");
     var localPassword = localStorage.getItem("password");
   }
+  const handleLogout = () => {
+    localStorage.removeItem("userName");
+    localStorage.removeItem("password");
+    window.location.reload();
+  };
   return (
-    <ChatEngine
-      height="100vh"
-      projectID="d426e579-2421-4e68-ba40-e01358bc8db1"
-      userName={localUser}
-      userSecret={localPassword}
-      // userName="bman"
-      // userSecret="bman123"
-      renderChatFeed={(renderChatProps) => <ChatFeed {...renderChatProps} />}
-    />
+    <React.Fragment>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "flex-end",
+          height: "4vh",
+          backgroundColor: "#F0F0F0",
+        }}
+      >
+        <button
+          onClick={handleLogout}
+          style={{
+            backgroundColor: "#99ccff",
+            color: "#ffff1a",
+            padding: "7px",
+            marginRight: "2rem",
+            border: "none",
+          }}
+        >
+          Logout
+        </button>
+      </div>
+      <ChatEngine
+        height="96vh"
+        projectID="d426e579-2421-4e68-ba40-e01358bc8db1"
+        userName={localUser}
+        userSecret={localPassword}
+        // userName="bman"
+        // userSecret="bman123"
+        renderChatFeed={(renderChatProps) => <ChatFeed {...renderChatProps} />}
+      />
+    </React.Fragment>
   );
 };
 
